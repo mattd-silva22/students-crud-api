16a626a7c6fcce35726dd8bf0f312dcbfa09cbc4
matheus.barretto <matheus.barretto2001@gmail.com>
2024-06-01 23:24:38 -0300

    refactor(students): remove NotFoundError from students modules

--- src/students/errors/NotFound.error.ts
+++
@@ -1,8 +0,0 @@
-import { StatusCodes } from "http-status-codes";
-import { DefaultError } from "src/shared/errors/DefaultError.error";
-
-export class NotFoundError extends DefaultError {
-  constructor(message: string, statusCode: number = StatusCodes.NOT_FOUND) {
-    super("NotFoundError", message, statusCode);
-  }
-}
